314311639a6506820c9b807851fe51799dfb2e91
Alec T. Musasa <alecmus@live.com>
2020-07-15 10:50:13 +0200

    Closing time widget properly implemented.

--- form_impl/form_impl.cpp
+++ form_impl/form_impl.cpp
@@ -1982,14 +1982,26 @@ namespace liblec {
 				}
 				catch (const std::exception&) {}
 
-				// check special html pane
-				auto& html_pane_control = container.d_page_.get_pane(widgets::pane_impl::html_pane_alias_prefix() + path);
+				try {
+					// check special html pane
+					auto& html_pane_control = container.d_page_.get_pane(widgets::pane_impl::html_pane_alias_prefix() + path);
 
-				// html pane control confirmed ... get the pane page
-				auto& html_pane = html_pane_control.p_panes_.at("pane");
+					// html pane control confirmed ... get the pane page
+					auto& html_pane = html_pane_control.p_panes_.at("pane");
+
+					// pane confirmed ... recurse
+					return find_widget(html_pane, path);
+				}
+				catch (const std::exception&) {}
+
+				// check special time pane
+				auto& time_pane_control = container.d_page_.get_pane(widgets::pane_impl::time_pane_alias_prefix() + path);
+
+				// time pane control confirmed ... get the pane page
+				auto& time_pane = time_pane_control.p_panes_.at("pane");
 
 				// pane confirmed ... recurse
-				return find_widget(html_pane, path);
+				return find_widget(time_pane, path);
 			}
 			else {
 				// get the container's alias
@@ -2203,8 +2215,16 @@ namespace liblec {
 						close_container(pane_path + widgets::pane_impl::html_pane_alias_prefix() + result.widget.alias());
 					}
 				} break;
-				case liblec::lecui::widgets::widget_type::time:
-					break;
+				case liblec::lecui::widgets::widget_type::time: {
+					// get the special pane's path
+					const auto idx = path.rfind("/");
+
+					if (idx != std::string::npos) {
+						const auto pane_path = path.substr(0, idx + 1);
+						// close time pane (this will automatically close all the contents, including the widget in question)
+						close_container(pane_path + widgets::pane_impl::time_pane_alias_prefix() + result.widget.alias());
+					}
+				} break;
 				case liblec::lecui::widgets::widget_type::date:
 					break;
 				case liblec::lecui::widgets::widget_type::icon:
