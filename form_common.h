a0294d89f3af0210376ec0c632efcd08dc9cf4a0
Alec Musasa <alecmus@live.com>
2021-06-21 16:09:24 +0200

    Added the trim function.

--- form_common.h
+++ form_common.h
@@ -594,5 +594,20 @@ namespace liblec {
 
 			return dpi_scale_;
 		}
+
+		static inline void trim(std::string& s) {
+			auto trim_left = [](std::string& s) {
+				s.erase(s.begin(), std::find_if(s.begin(), s.end(),
+					[](unsigned char ch) { return !std::isspace(ch); }));
+			};
+
+			auto trim_right = [](std::string& s) {
+				s.erase(std::find_if(s.rbegin(), s.rend(),
+					[](unsigned char ch) { return !std::isspace(ch); }).base(), s.end());
+			};
+
+			trim_left(s);
+			trim_right(s);
+		}
 	}
 }
