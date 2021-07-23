alecmus/lecui

commit 236819221b2fc366208658ead04f7fcdadbaab74

Added arrows in table view to show sorting order.

widgets/table_view/table_view_impl.cpp
@@ -258,7 +258,63 @@ namespace liblec {
 						}
 					}
 
+					auto right_limit = rect_header_cell.right;
+
+					// check if there are sort options
+					if (header_sort_options_.count(it.name)) {
+						// make a reference rectangle
+						auto reference = rect_header_cell;
+						const auto side = reference.bottom - reference.top;
+
+						// make it a square
+						reference.left = reference.right - side;
+
+						// make our target rectangle
+						// width 40%
+						// height 25%
+						D2D1_RECT_F rect = { 0.f, 0.f, 8.f, 5.f };
+
+						// position the square to the middle of the reference
+						pos_rect(reference, rect, 50.f, 50.f);
+
+						switch (header_sort_options_.at(it.name)) {
+						case liblec::lecui::sort_options::ascending: {
+							// draw marker pointing upwards to show ascent
+
+							D2D1_POINT_2F bottom_left, bottom_right, top;
+							bottom_left = { rect.left, rect.bottom };
+							bottom_right = { rect.right, rect.bottom };
+							top = { rect.left + (rect.right - rect.left) / 2.f, rect.top };
+
+							// draw arrow
+							p_render_target->DrawLine(bottom_left, top, hot ? p_brush_text_header_hot_ : p_brush_text_header_);
+							p_render_target->DrawLine(bottom_right, top, hot ? p_brush_text_header_hot_ : p_brush_text_header_);
+
+							right_limit = rect.left;
+						} break;
+
+						case liblec::lecui::sort_options::descending: {
+							// draw marker pointing downwards to show descent
+
+							D2D1_POINT_2F top_left, top_right, bottom;
+							top_left = { rect.left, rect.top };
+							top_right = { rect.right, rect.top };
+							bottom = { rect.left + (rect.right - rect.left) / 2.f, rect.bottom };
+
+							// draw arrow
+							p_render_target->DrawLine(top_left, bottom, hot ? p_brush_text_header_hot_ : p_brush_text_header_);
+							p_render_target->DrawLine(top_right, bottom, hot ? p_brush_text_header_hot_ : p_brush_text_header_);
+
+							right_limit = rect.left;
+						} break;
+						case liblec::lecui::sort_options::none:
+						default:
+							break;
+						}
+					}
+
 					auto rect_text = rect_header_cell;
+					rect_text.right = right_limit;
 					rect_text.left += margin_;
 					rect_text.right -= margin_;
 

commit ba668e45ac61f6ef71da1e9f430057f1f0eeb92e

Fix to bugs introduced in 236819221b2fc366208658ead04f7fcdadbaab74 and d06a408827f8f7218da6f2ffa6d454221ba28503 thanks to @tmnyoni for finding the issue.

versioninfo.h
@@ -22,4 +22,4 @@
 #define lecuiminor			0
 #define lecuirevision		0
 
-#define lecuidate			"22 Jul 2021"
+#define lecuidate			"23 Jul 2021"

widgets/table_view/table_view_impl.cpp
@@ -280,8 +280,9 @@ namespace liblec {
 					}
 
 					// draw the vertical divider
-					p_render_target->DrawLine(D2D1::Point2F(rect_header_cell.right, rect_header_cell.top),
-						D2D1::Point2F(rect_header_cell.right, rect_header_cell.bottom), _p_brush_grid, _specs.grid_line());
+					if (render && _visible)
+						p_render_target->DrawLine(D2D1::Point2F(rect_header_cell.right, rect_header_cell.top),
+							D2D1::Point2F(rect_header_cell.right, rect_header_cell.bottom), _p_brush_grid, _specs.grid_line());
 
 					auto right_limit = rect_header_cell.right;
 
@@ -312,8 +313,10 @@ namespace liblec {
 							top = { rect.left + (rect.right - rect.left) / 2.f, rect.top };
 
 							// draw arrow
-							p_render_target->DrawLine(bottom_left, top, hot ? _p_brush_text_header_hot : _p_brush_text_header);
-							p_render_target->DrawLine(bottom_right, top, hot ? _p_brush_text_header_hot : _p_brush_text_header);
+							if (render && _visible) {
+								p_render_target->DrawLine(bottom_left, top, hot ? _p_brush_text_header_hot : _p_brush_text_header);
+								p_render_target->DrawLine(bottom_right, top, hot ? _p_brush_text_header_hot : _p_brush_text_header);
+							}
 
 							right_limit = rect.left;
 						} break;
@@ -327,8 +330,10 @@ namespace liblec {
 							bottom = { rect.left + (rect.right - rect.left) / 2.f, rect.bottom };
 
 							// draw arrow
-							p_render_target->DrawLine(top_left, bottom, hot ? _p_brush_text_header_hot : _p_brush_text_header);
-							p_render_target->DrawLine(top_right, bottom, hot ? _p_brush_text_header_hot : _p_brush_text_header);
+							if (render && _visible) {
+								p_render_target->DrawLine(top_left, bottom, hot ? _p_brush_text_header_hot : _p_brush_text_header);
+								p_render_target->DrawLine(top_right, bottom, hot ? _p_brush_text_header_hot : _p_brush_text_header);
+							}
 
 							right_limit = rect.left;
 						} break;
@@ -364,8 +369,9 @@ namespace liblec {
 			}
 
 			// draw the horizontal divider
-			p_render_target->DrawLine(D2D1::Point2F(_rect_header.left, _rect_header.bottom),
-				D2D1::Point2F(_rect_header.right, _rect_header.bottom), _p_brush_grid, _specs.grid_line());
+			if (render && _visible)
+				p_render_target->DrawLine(D2D1::Point2F(_rect_header.left, _rect_header.bottom),
+					D2D1::Point2F(_rect_header.right, _rect_header.bottom), _p_brush_grid, _specs.grid_line());
 
 			// step11: draw table
 			{