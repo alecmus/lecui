117093b88f8e689bf7af3876a443dd24a7928555
Alec Musasa <alecmus@live.com>
2021-12-18 18:01:54 +0200

    Updated license information to match the liblec update

--- menus/form_menu/form_menu.cpp
+++ menus/form_menu/form_menu.cpp
@@ -4,8 +4,8 @@
 // lecui user interface library, part of the liblec library
 // Copyright (c) 2019 Alec Musasa (alecmus at live dot com)
 //
-// Released under the CC-BY-NC 2.0 license. For full details see the file
-// LICENSE.txt or go to https://github.com/alecmus/liblec/blob/master/LICENSE.md
+// Released under the MIT license. For full details see the
+// file LICENSE.txt
 //
 
 #include "../form_menu.h"
