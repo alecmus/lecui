alecmus/lecui

commit b07f9f544d257918ad1d86e3bc91d4e10568d756

Added the change event to text fields and password fields.

versioninfo.h
@@ -22,4 +22,4 @@
 #define lecuiminor			0
 #define lecuirevision		0
 
-#define lecuidate			"26 Dec 2021"
+#define lecuidate			"28 Dec 2021"

widgets/text_field/text_field_impl.cpp
@@ -376,6 +376,9 @@ namespace liblec {
 				_caret_position++;
 				_caret_visible = true;
 				_skip_blink = true;
+
+				if (_specs.events().change)
+					_specs.events().change(_specs.text());
 			}
 			catch (const std::exception& e) { log(e.what()); }
 		}
@@ -398,6 +401,9 @@ namespace liblec {
 					_caret_visible = true;
 					_skip_blink = true;
 				}
+
+				if (_specs.events().change)
+					_specs.events().change(_specs.text());
 			}
 			catch (const std::exception& e) { log(e.what()); }
 		}
@@ -419,6 +425,9 @@ namespace liblec {
 					_caret_visible = true;
 					_skip_blink = true;
 				}
+
+				if (_specs.events().change)
+					_specs.events().change(_specs.text());
 			}
 			catch (const std::exception& e) { log(e.what()); }
 		}

widgets/text_field.h
@@ -42,6 +42,17 @@ namespace liblec {
 					_rect.size({ 200.f, 25.f });
 				}
 
+				/// <summary>Events specific to this widget.</summary>
+				struct text_field_events : basic_events {
+					/// <summary>Handler that's called when the text changes.</summary>
+					/// <remarks>The parameter contains the new text.</remarks>
+					std::function<void(const std::string&)> change = nullptr;
+				};
+
+				text_field_events& events() {
+					return _text_field_events;
+				}
+
 				bool operator==(const text_field&);
 				bool operator!=(const text_field&);
 
@@ -209,6 +220,8 @@ namespace liblec {
 			private:
 				/// <summary>The character to use for masking the text.</summary>
 				char mask = '\0';
+
+				text_field_events _text_field_events;
 
 #if defined(LECUI_EXPORTS)
 				friend class password_field_builder;

commit 70a6ef63779a5920b9e606d257c074f333207d5c

Bug-fix: action handler for text_field now fixed (was broken by b07f9f544d257918ad1d86e3bc91d4e10568d756).

menus/context_menu.h
@@ -23,7 +23,7 @@ namespace liblec {
 			/// <summary>The label text of the menu item. Use an empty string to make a separator.</summary>
             std::string label;
 
-			/// <summary>The image to use for the menu item (placed on the right).</summary>
+			/// <summary>The image to use for the menu item (placed on the left).</summary>
             std::string image_file;
 
 			/// <summary>The font to use.</summary>

versioninfo.h
@@ -22,4 +22,4 @@
 #define lecuiminor			0
 #define lecuirevision		0
 
-#define lecuidate			"28 Dec 2021"
+#define lecuidate			"30 Dec 2021"

widgets/text_field/text_field_impl.cpp
@@ -331,6 +331,11 @@ namespace liblec {
 			}
 		}
 
+		void widgets::text_field_impl::on_action() {
+			if (_specs.events().action)
+				_specs.events().action();
+		}
+
 		widgets::text_field&
 			widgets::text_field_impl::specs() { return _specs; }
 

widgets/text_field/text_field_impl.h
@@ -37,6 +37,7 @@ namespace liblec {
 					const D2D1_SIZE_F& change_in_size, const D2D1_POINT_2F& offset,
 					const bool& render) override;
 				void on_selection_change(const bool& selected) override;
+				void on_action() override;
 
 				/// widget specific methods
 				widgets::text_field& specs();