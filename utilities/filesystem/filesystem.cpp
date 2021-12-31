c72788835de1b031504f5acca9fe32c3394521fa
Alec Musasa <alecmus@live.com>
2021-12-31 15:10:18 +0200

    Upgraded from the outdated GetOpenFileNameA to the modern IFileDialog.

--- utilities/filesystem/filesystem.cpp
+++ utilities/filesystem/filesystem.cpp
@@ -154,27 +154,177 @@ namespace liblec {
 			return;
 		}
 
-		std::string filesystem::open_file(const open_file_params& params) {
-			std::vector<std::string> filetypes_indexed;
-			const auto filter_string =
-				make_filter_string(params.file_types, params.include_all_supported_types,
-					filetypes_indexed);
+		/// <summary>Helper class for handling COMDLG_FILTERSPEC filters.</summary>
+		class filter_helper {
+			COMDLG_FILTERSPEC* _filters = nullptr;
+			DWORD _num_extensions = 0;
+			filter_helper() = delete;
 
-			char _FilePath[MAX_PATH];
-			OPENFILENAMEA ofn;
-			ZeroMemory(&ofn, sizeof(ofn));
-			ofn.lStructSize = sizeof(ofn);
-			ofn.hwndOwner = _d._fm._d._hWnd;
-			ofn.lpstrFile = _FilePath;
-			ofn.lpstrFile[0] = '\0';
-			ofn.nMaxFile = MAX_PATH;
-			ofn.lpstrFilter = filter_string.c_str();
-			ofn.nFilterIndex = 1;
-			ofn.hInstance = GetModuleHandle(NULL);
-			ofn.lpstrTitle = params.title.c_str();
-			ofn.Flags = OFN_ENABLEHOOK | OFN_EXPLORER | OFN_HIDEREADONLY | OFN_PATHMUSTEXIST |
-				OFN_FILEMUSTEXIST | OFN_ENABLESIZING;
-			return (GetOpenFileNameA(&ofn) == TRUE) ? std::string(_FilePath) : std::string();
+		public:
+			/// <summary>Constructor.</summary>
+			/// <param name="types">The list of file types.</param>
+			/// <param name="include_all_supported_types">Whether to include an "All supported types" option.</param>
+			filter_helper(std::vector<file_type> types, bool include_all_supported_types) {
+				// make filter
+				_filters = make_filter(types, _num_extensions, include_all_supported_types);
+			}
+
+			~filter_helper() {
+				// clean-up
+				free_filter(_filters);
+			}
+
+			/// <summary>Get the filter.</summary>
+			/// <returns>The filter.</returns>
+			COMDLG_FILTERSPEC* get_filter() {
+				return _filters;
+			}
+			
+			/// <summary>Get the size of the filter.</summary>
+			/// <returns>The size of the filter.</returns>
+			DWORD get_filter_size() {
+				return _num_extensions;
+			}
+
+		private:
+			COMDLG_FILTERSPEC* make_filter(std::vector<file_type> types, DWORD& num_extensions, bool include_all_supported_types) {
+				num_extensions = 0;
+
+				// use a map to group extensions by description
+				std::map<std::string, file_type> _types;
+
+				for (const auto& type : types) {
+					if (_types.count(type.description))
+						_types.at(type.description).extension += ";*." + type.extension;
+					else
+						_types.insert(std::make_pair(type.description, type));
+				}
+
+				if (!_types.empty()) {
+					DWORD index = (include_all_supported_types && _types.size() > 1) ? 1 : 0;
+
+					// dynamic memory allocation (for filter list)
+					auto* filters = new COMDLG_FILTERSPEC[_types.size() + 1 + index];
+
+					size_t all_spec_length = 0;
+
+					for (const auto& [description, type] : _types) {
+						std::wstring description_string = convert_string(type.description);
+						std::wstring spec_string = convert_string("*." + type.extension);
+
+						WCHAR* description_source = (WCHAR*)description_string.c_str();
+						WCHAR* spec_source = (WCHAR*)spec_string.c_str();
+
+						// dynamic memory allocation (name and spec strings)
+						WCHAR* description_buffer = (WCHAR*)std::malloc((wcslen(description_source) + 1) * sizeof(WCHAR));
+						WCHAR* spec_buffer = (WCHAR*)std::malloc((wcslen(spec_source) + 1) * sizeof(WCHAR));
+
+						if (description_buffer && spec_buffer) {
+							memcpy(description_buffer, description_source, (wcslen(description_source) + 1) * sizeof(WCHAR));
+							memcpy(spec_buffer, spec_source, (wcslen(spec_source) + 1) * sizeof(WCHAR));
+
+							filters[index].pszName = description_buffer;
+							filters[index].pszSpec = spec_buffer;
+
+							all_spec_length += wcslen(filters[index].pszSpec) + 1;
+						}
+						else {
+							filters[index].pszName = nullptr;
+							filters[index].pszSpec = nullptr;
+							break;
+						}
+
+						index++;
+					}
+
+					if (include_all_supported_types && _types.size() > 1) {
+						const WCHAR* all_supported_file_types_string = L"All supported files";
+
+						// dynamic memory allocation (name and spec strings)
+						WCHAR* description_buffer = (WCHAR*)std::malloc((wcslen(all_supported_file_types_string) + 1) * sizeof(WCHAR));
+						WCHAR* spec_buffer = (WCHAR*)std::malloc(all_spec_length * sizeof(WCHAR));
+
+						if (description_buffer && spec_buffer) {
+							wcscpy_s(description_buffer, wcslen(all_supported_file_types_string) + 1, all_supported_file_types_string);
+							spec_buffer[0] = 0;
+
+							for (DWORD j = 1; j < index; j++) {
+								wcscat_s(spec_buffer, all_spec_length, filters[j].pszSpec);
+								if (j != index - 1)
+									wcscat_s(spec_buffer, all_spec_length, L";");
+							}
+						}
+
+						filters[0].pszName = description_buffer;
+						filters[0].pszSpec = spec_buffer;
+					}
+
+					num_extensions = index;
+					filters[index].pszName = nullptr;
+					filters[index].pszSpec = nullptr;
+					return filters;
+				}
+				else
+					return nullptr;
+			}
+
+			void free_filter(COMDLG_FILTERSPEC* filters) {
+				if (filters) {
+					int32_t index = 0;
+					while (filters[index].pszName) {
+						// free memory allocated for name and spec strings
+						std::free((void*)filters[index].pszName);
+						std::free((void*)filters[index].pszSpec);
+						index++;
+					}
+
+					// free memory allocated for filter list
+					delete[] filters;
+					filters = nullptr;
+				}
+			}
+		};
+
+		std::string filesystem::open_file(const open_file_params& params) {
+			std::string file_path;
+
+			IFileDialog* p_file_dialog = nullptr;
+
+			if (SUCCEEDED(CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_INPROC_SERVER,
+				IID_PPV_ARGS(&p_file_dialog)))) {
+
+				DWORD options;
+				if (SUCCEEDED(p_file_dialog->GetOptions(&options))) {
+					p_file_dialog->SetOptions(options | FOS_FORCEFILESYSTEM);
+
+					filter_helper helper(params.file_types, params.include_all_supported_types);
+					p_file_dialog->SetFileTypes(helper.get_filter_size(), helper.get_filter());
+
+					if (!params.title.empty())
+						p_file_dialog->SetTitle(convert_string(params.title).c_str());
+
+					if (SUCCEEDED(p_file_dialog->Show(_d._fm._d._hWnd))) {
+
+						IShellItem* p_shell_item = nullptr;
+
+						if (SUCCEEDED(p_file_dialog->GetResult(&p_shell_item))) {
+							LPWSTR result = nullptr;
+							p_shell_item->GetDisplayName(SIGDN_FILESYSPATH, &result);
+							p_shell_item->Release();
+
+							if (result)
+								file_path = convert_string(std::wstring(result));
+
+							if (result != NULL)
+								CoTaskMemFree(result);
+						}
+					}
+				}
+
+				p_file_dialog->Release();
+			}
+
+			return file_path;
 		}
 
 		std::string filesystem::save_file(const std::string& file, const save_file_params& params) {
