alecmus/lecui

commit 5e62abfe1b05fed1922793959b9d66e745ed505b

Bug-fix: programmatic change in size in panes and tab_panes now being seen by the widgets inside them.

form_impl/paint/on_render.cpp
@@ -310,9 +310,16 @@ namespace liblec {
 										// get client area for this tab pane
 										const auto& client_area = tab_pane.client_area();
 
-										const D2D1_SIZE_F change_in_size = {
+										const D2D1_SIZE_F resizing_change_in_size = {
 											(tab_pane.tab_pane_area().right - tab_pane.tab_pane_area().left) - tab_pane().rect().width(),
 											(tab_pane.tab_pane_area().bottom - tab_pane.tab_pane_area().top) - tab_pane().rect().height()
+										};
+
+										const D2D1_SIZE_F programmatic_change_in_size = tab_pane.programmatic_change_in_size();
+
+										const D2D1_SIZE_F change_in_size = {
+											resizing_change_in_size.width + programmatic_change_in_size.width,
+											resizing_change_in_size.height + programmatic_change_in_size.height
 										};
 
 										for (auto& tab : tab_pane._p_tabs) {
@@ -340,9 +347,16 @@ namespace liblec {
 											// get client area for this pane
 											const auto& client_area = pane.client_area();
 
-											const D2D1_SIZE_F change_in_size = {
+											const D2D1_SIZE_F resizing_change_in_size = {
 												(pane.pane_area().right - pane.pane_area().left) - pane().rect().width(),
 												(pane.pane_area().bottom - pane.pane_area().top) - pane().rect().height()
+											};
+
+											const D2D1_SIZE_F programmatic_change_in_size = pane.programmatic_change_in_size();
+
+											const D2D1_SIZE_F change_in_size = {
+												resizing_change_in_size.width + programmatic_change_in_size.width,
+												resizing_change_in_size.height + programmatic_change_in_size.height
 											};
 
 											for (auto& page : pane._p_panes) {

containers/pane/pane_impl.cpp
@@ -262,6 +262,7 @@ namespace liblec {
 			}
 
 			if (!_initial_capture_done) {
+				_rect_original = _specs.rect();
 				_rect_previous = _specs.rect();
 				_initial_capture_done = true;
 			}
@@ -343,6 +344,13 @@ namespace liblec {
 		const D2D1_RECT_F& widgets::pane_impl::pane_area() { return _rect_pane; }
 
 		const float& widgets::pane_impl::content_margin() { return _content_margin; }
+
+		const D2D1_SIZE_F widgets::pane_impl::programmatic_change_in_size() {
+			auto& _specs = _p_panes.at(_current_pane);
+			const auto& width_change = _specs.rect().width() - _rect_original.width();
+			const auto& height_change = _specs.rect().height() - _rect_original.height();
+			return { width_change, height_change };
+		}
 
 		bool widgets::pane_impl::contains() { return false; }
 	}

containers/pane/pane_impl.h
@@ -83,6 +83,7 @@ namespace liblec {
 				const D2D1_RECT_F& client_area();
 				const D2D1_RECT_F& pane_area();
 				const float& content_margin();
+				const D2D1_SIZE_F programmatic_change_in_size();
 
 			private:
 				// Default constructor and copying an object of this class are not allowed
@@ -100,9 +101,11 @@ namespace liblec {
 				const float _content_margin;
 				D2D1_RECT_F _rect_client_area;
 				D2D1_RECT_F _rect_pane;
+				D2D1_SIZE_F _programmatic_change_in_size = { 0.f, 0.f };
 
 				bool _initial_capture_done = false;
 				rect _rect_previous = { 0.f, 0.f, 0.f, 0.f };
+				rect _rect_original = { 0.f, 0.f, 0.f, 0.f };
 
 				/// Important override for pane to work properly.
 				bool contains() override;

containers/tab_pane/tab_pane_impl.cpp
@@ -167,6 +167,7 @@ namespace liblec {
 			}
 
 			if (!_initial_capture_done) {
+				_rect_original = _specs.rect();
 				_rect_previous = _specs.rect();
 				_initial_capture_done = true;
 			}
@@ -741,6 +742,12 @@ namespace liblec {
 				_tab_order = temp_tab_order;
 			}
 			catch (const std::exception&) {}
+		}
+
+		const D2D1_SIZE_F widgets::tab_pane_impl::programmatic_change_in_size() {
+			const auto& width_change = _specs.rect().width() - _rect_original.width();
+			const auto& height_change = _specs.rect().height() - _rect_original.height();
+			return { width_change, height_change };
 		}
 
 		bool widgets::tab_pane_impl::contains() {

containers/tab_pane/tab_pane_impl.h
@@ -71,6 +71,7 @@ namespace liblec {
 				void set_tab_height(const float height);
 				bool tab_height_set();
 				void close_tab(const std::string& tab_name);
+				const D2D1_SIZE_F programmatic_change_in_size();
 
 			private:
 				// Default constructor and copying an object of this class are not allowed
@@ -106,6 +107,7 @@ namespace liblec {
 
 				bool _initial_capture_done = false;
 				rect _rect_previous = { 0.f, 0.f, 0.f, 0.f };
+				rect _rect_original = { 0.f, 0.f, 0.f, 0.f };
 
 				/// Important override for tab pane to work properly.
 				bool contains();

commit a3915a00c7899d8aa8b59c69e3f2cced4ce803be

Fixed scrollbar bug introduced in 5e62abfe1b05fed1922793959b9d66e745ed505b.

form_impl/paint/on_render.cpp
@@ -73,6 +73,7 @@ namespace liblec {
 						const D2D1_RECT_F& rectB,
 						const D2D1_RECT_F& client_area,
 						const D2D1_SIZE_F& change_in_size,
+						const D2D1_SIZE_F& resizing_change_in_size,
 						const float& _dpi_scale,
 						ID2D1SolidColorBrush* _p_brush_theme,
 						ID2D1SolidColorBrush* _p_brush_theme_hot,
@@ -192,7 +193,7 @@ namespace liblec {
 										continue;
 
 									_rect_widgets = widget.render(_p_render_target,
-										change_in_size,
+										resizing_change_in_size,
 										{ (page._d_page.h_scrollbar()._x_off_set / _dpi_scale) - client_area.left,
 										(page._d_page.v_scrollbar()._y_off_set / _dpi_scale) - client_area.top },
 										false);
@@ -369,7 +370,7 @@ namespace liblec {
 											rect_page.bottom -= _content_margin;
 
 											render_page(render ? tab_pane.visible() : false, tab.first, tab_pane.specs().selected(), tab.second,
-												_p_render_target, rect_page, rect_page, change_in_size,
+												_p_render_target, rect_page, rect_page, change_in_size, resizing_change_in_size,
 												_dpi_scale, _p_brush_theme, _p_brush_theme_hot, lbutton_pressed, update_form);	// recursion
 										}
 									}
@@ -406,7 +407,7 @@ namespace liblec {
 												rect_page.bottom -= _content_margin;
 
 												render_page(render ? pane.visible() : false, page.first, pane._current_pane, page.second,
-													_p_render_target, rect_page, rect_page, change_in_size,
+													_p_render_target, rect_page, rect_page, change_in_size, resizing_change_in_size,
 													_dpi_scale, _p_brush_theme, _p_brush_theme_hot, lbutton_pressed, update_form);	// recursion
 											}
 										}
@@ -426,13 +427,13 @@ namespace liblec {
 						// setup horizontal scroll bar and render it
 						page._d_page.h_scrollbar().setup(rectA, rectB);
 						page._d_page.h_scrollbar().render(_p_render_target,
-							change_in_size, { 0.f - client_area.left,
+							resizing_change_in_size, { 0.f - client_area.left,
 							0.f - client_area.top }, render);
 
 						// setup vertical scroll bar and render it
 						page._d_page.v_scrollbar().setup(rectA, rectB);
 						page._d_page.v_scrollbar().render(_p_render_target,
-							change_in_size, { 0.f - client_area.left,
+							resizing_change_in_size, { 0.f - client_area.left,
 							0.f - client_area.top }, render);
 
 						if (page._d_page.h_scrollbar()._programmatic_h_scroll) {
@@ -472,7 +473,7 @@ namespace liblec {
 					const D2D1_RECT_F client_area = rect_page;
 
 					helper::render_page(true, page.first, _current_page, page.second, _p_render_target,
-						rect_page, client_area, change_in_size, _dpi_scale,
+						rect_page, client_area, change_in_size, change_in_size, _dpi_scale,
 						_p_brush_theme, _p_brush_theme_hot, _lbutton_pressed, update_form);
 				}
 
@@ -488,7 +489,7 @@ namespace liblec {
 						const D2D1_RECT_F client_area = rect_page;
 
 						helper::render_page(true, page.first, page.first, page.second, _p_render_target,
-							rect_page, client_area, change_in_size, _dpi_scale,
+							rect_page, client_area, change_in_size, change_in_size, _dpi_scale,
 							_p_brush_theme, _p_brush_theme_hot, _lbutton_pressed, update_form);
 					}
 
@@ -502,7 +503,7 @@ namespace liblec {
 						const D2D1_RECT_F client_area = rect_page;
 
 						helper::render_page(true, page.first, page.first, page.second, _p_render_target,
-							rect_page, client_area, change_in_size, _dpi_scale,
+							rect_page, client_area, change_in_size, change_in_size, _dpi_scale,
 							_p_brush_theme, _p_brush_theme_hot, _lbutton_pressed, update_form);
 					}
 
@@ -516,7 +517,7 @@ namespace liblec {
 						const D2D1_RECT_F client_area = rect_page;
 
 						helper::render_page(true, page.first, page.first, page.second, _p_render_target,
-							rect_page, client_area, change_in_size, _dpi_scale,
+							rect_page, client_area, change_in_size, change_in_size, _dpi_scale,
 							_p_brush_theme, _p_brush_theme_hot, _lbutton_pressed, update_form);
 					}
 
@@ -530,7 +531,7 @@ namespace liblec {
 						const D2D1_RECT_F client_area = rect_page;
 
 						helper::render_page(true, page.first, page.first, page.second, _p_render_target,
-							rect_page, client_area, change_in_size, _dpi_scale,
+							rect_page, client_area, change_in_size, change_in_size, _dpi_scale,
 							_p_brush_theme, _p_brush_theme_hot, _lbutton_pressed, update_form);
 					}
 				}

versioninfo.h
@@ -22,4 +22,4 @@
 #define lecuiminor			0
 #define lecuirevision		0
 
-#define lecuidate			"15 Mar 2022"
+#define lecuidate			"16 Mar 2022"